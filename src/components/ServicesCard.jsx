d37123a2b5d18b776b4f71e4a62f65a4bd773eca
khomabhena <colwanymab@gmail.com>
2025-08-14 18:59:33 +0200

    making changes

--- src/components/ServicesCard.jsx
+++ src/components/ServicesCard.jsx
@@ -5,7 +5,7 @@ const ServicesCard = ({ image, title, text }) => {
   return (
     <section style={{ backgroundColor: colors.backgroundColor }} className=' rounded-xl py-8 shadow-sm md:shadow-lg basis-1/4 flex md:h-48 gap-8 w-full '>
       <img className=' mx-4 w-12 md:w-16 lg:w-20 self-center' src={image} alt={title} />
-      <div className=' flex flex-col w-[350px] md:w-[400px] lg:w-[500px] justify-center pr-2'>
+      <div className=' flex flex-col pb-8 w-[350px] md:w-[400px] lg:w-[450px] justify-center pr-2'>
         <h3 className=' text-sm md:text-lg font-bold'>{title}</h3>
         <p className=' text-sm md:text-lg text-gray-700'>{text}</p>
       </div>
--- src/sections/AboutUs2.jsx
+++ src/sections/AboutUs2.jsx
@@ -10,7 +10,7 @@ const AboutUs2 = () => {
 
     <section className=' flex flex-col w-full items-center pb-24 mt-32 justify-center'>
         <h2 className=' text-4xl font-medium'>{heading}</h2>
-        <div className=' w-full flex flex-wrap mt-12 gap-4 md:gap-0 flex-col xl:flex-row justify-center'>
+        <div className=' w-full flex flex-wrapl mt-12 gap-4 md:gap-0 flex-col xl:flex-row justify-center'>
           {
             data?.map(({ title, subtitle, text, points }) => (
                 <AboutUsCard2 title={title} subtitle={subtitle} text={text} points={points} />
