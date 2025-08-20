92d3c4582beebedfebd556acbfadfe21da17dd0d
khomabhena <colwanymab@gmail.com>
2025-08-20 14:14:23 +0200

    added navigation data

--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -1,10 +1,12 @@
 import React, { useContext } from 'react'
 import { colors } from '../data/colors'
 import NavigationContext from '../context/NavigationContext'
+import { getNavigationData } from '../data/navigation'
 
 const MainNavigation = ({ prefix = '' }) => {
     const {mobileNavigation, setMobileNavigation} = useContext(NavigationContext)
    const primaryColor = colors.primaryColor
+   const data = getNavigationData
 
   return ( 
     <nav style={{ boxShadow: colors.primaryColor, backgroundColor: colors.backgroundColor }} className=' fixed shadow-[#faa819]/30l shadow-lg sha top-0 left-0 right-0 m-auto w-full max-w-[1920px] z-20 flex justify-between items-center px-8 md:px-12 lg:px-24 2xl:px-32 h-16 md:h-32'>
@@ -14,8 +16,13 @@ const MainNavigation = ({ prefix = '' }) => {
       </a>
 
       <ul className=' hidden lg:flex gap-8 text-base pr-4' style={{ color: primaryColor }}>
-        <li className=' px-8 py-2 rounded-lg text-lg hover:scale-105 hover:shadow-gray-300 hover:border-green-900 hover:bg-[#faa819] hover:text-gray-50 hover:shadow-xl ease-in-out transition-all'><a href={`${prefix}#personal`}>Personal</a></li>
-        <li className=' px-8 py-2 rounded-lg text-lg hover:scale-105 hover:shadow-gray-300 hover:border-green-900 hover:bg-[#faa819] hover:text-gray-50 hover:shadow-xl ease-in-out transition-all'><a href={`${prefix}#business`}>Business</a></li>
+        {
+          data.map(({ link, name}) => (
+            <li key={name} className=' px-8 py-2 rounded-lg text-lg hover:scale-105 hover:shadow-gray-300 hover:bg-[#faa819] hover:text-gray-50 hover:shadow-xl ease-in-out transition-all'>
+              <a href={`${prefix}${link}`}>{name}</a>
+            </li>
+          ))
+        }
         <li style={{ borderColor: primaryColor }} className=' border-3  px-4 py-2 rounded-lg text-lg hover:scale-105 hover:shadow-gray-300 hover:border-green-900 hover:bg-[#faa819] hover:text-gray-50 hover:shadow-xl ease-in-out transition-all'><a href={`${prefix}#internet-banking`}>Internet Banking</a></li>
       </ul>
 
---
+++ src/data/navigation.js
@@ -0,0 +1,14 @@
+export const getNavigationData = [
+    {
+        link: '',
+        name: 'Personal',
+    },
+    {
+        link: '',
+        name: 'Business',
+    },
+    {
+        link: '',
+        name: 'About Us',
+    },
+]
