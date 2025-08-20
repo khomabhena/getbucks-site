e26be07f1b775441cc132f7e2fa29edeec53dc7c
khomabhena <colwanymab@gmail.com>
2025-08-20 15:56:24 +0200

    text resizing on landing page

--- src/sections/LandingPageContent.jsx
+++ src/sections/LandingPageContent.jsx
@@ -17,8 +17,8 @@ const LandingPageContent = ({  }) => {
           <div className={` ${index % 2 == 0 ? 'xl:flex-row-reverse' : 'xl:flex-row'} flex flex-col items-center justify-between gap-12 xl:gap-24 mt-12`}>
             
             <section className=' flex flex-col basis-1 md:basis-2/4 xl:px-12'>
-              <h2 className=' text-3xl font-semibold'>{title}</h2>
-              <p className=' text-gray-700 mt-6'>{description}</p>
+              <h2 className=' text-lg xl:text-3xl font-semibold'>{title}</h2>
+              <p className=' text-sm xl:text-base text-gray-700 mt-6'>{description}</p>
             </section>
 
             <div className=' basis-1 md:basis-2/4 xl:pl-12'>
