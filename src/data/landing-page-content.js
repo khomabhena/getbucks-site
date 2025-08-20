8eb8e32197ab6176e05e0dde78661da03161b0ec
Kholwani Mabhena <kholwani@appleseed.tech>
2025-08-20 09:13:26 +0200

    added landing page content and data

---
+++ src/data/landing-page-content.js
@@ -0,0 +1,39 @@
+export const landingPageContent = [
+    {
+        image: "./",
+        title: "Instant Notifications",
+        description: "Get real time alerts every time money moves in or out of your account. From deposits to purchases, our smart notifications keep you informed, in control, and protected",
+
+    },
+    {
+        image: "./",
+        title: "Insurance",
+        description: "From health to travel to assets, our insurance solutions are built into your banking experience.",
+
+    },
+    {
+        image: "./",
+        title: "Loans",
+        description: "Get access to credit faster than ever. With flexible repayment options, our loans are designed to power your personal or business goals.",
+    },
+    {
+        image: "./",
+        title: "Saving",
+        description: "So your money grows while you live your life. build your future with intelligent savings tools. Automate deposits, set goals, and track progress in real time",
+    },
+    {
+        image: "./",
+        title: "Credit & Debit Cards",
+        description: "Get more from every swipe. With built in security and rewards designed around your lifestyle, our credit cards give you fexibility and fnancial power.",
+    },
+    {
+        image: "./",
+        title: "Security",
+        description: "From biometric logins to fraud detection, your money is always protected. We use next-gen encryption and real-time monitoring to keep your banking safe, so you can focus on what matters.",
+    },
+    {
+        image: "./",
+        title: "Cash Back",
+        description: "Get value in every transaction, with purchases that pays you back.",
+    },
+]
