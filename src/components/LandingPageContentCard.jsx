7a00b2159a9e82fcee36c0a6e4f62634dde36694
khomabhena <colwanymab@gmail.com>
2025-08-21 14:18:08 +0200

    switched the text alignment

--- src/components/LandingPageContentCard.jsx
+++ src/components/LandingPageContentCard.jsx
@@ -4,9 +4,9 @@ import { colors } from '../data/colors'
 const LandingPageContentCard = ({ index, image }) => {
 
   return (
-    <div className={` relative ${index % 2 == 0 ? 'justify-start' : 'justify-end'} flex w-full min-h-64 mx-10l`}>
+    <div className={` relative ${index % 2 == 0 ? 'justify-start' : 'justify-end'} flex w-full h-fit mx-10l`}>
 
-      <div className=' relative flex flex-col justify-center mx-10l rounded-4xl top-0 left-0 xl:w-[500px] min-h-52 shadow-lg hover:shadow-2xl shadow-[#faa819] bg-gradient-to-r from-gray-100 to-gray-200 hover:scale-104 ease-in-out transition-all'>
+      <div style={{ backgroundColor: colors.backgroundColor}} className=' relative flex flex-col justify-center mx-10l rounded-4xl top-0 left-0 xl:w-[500px] min-h-52 shadow-lg hover:shadow-2xl shadow-[#faa819] bg-gradient-to-r from-gray-100d to-gray-200d hover:scale-104 ease-in-out transition-all'>
         <div className=' absolute -mt-5 -ml-5 md:-mt-6 md:-ml-6 top-0 left-0 w-32 h-32'>
           <img className=' w-64' src="/about/icon-01.png" alt="" />
         </div>
@@ -15,7 +15,7 @@ const LandingPageContentCard = ({ index, image }) => {
           <img src="/about/icon-010.png" alt="" />
         </div>
 
-        <img className=' rounded-4xl xl:min-h-64 object-cover xl:h-[200px]' src={image} />
+        <img className=' rounded-4xl xl:max-h-64 object-cover h-[200px] xl:h-[300px]' src={image} />
       </div>
 
     </div>
