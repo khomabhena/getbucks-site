6e69e90b6f1388bda602ae8553f035d4f9709fc9
khomabhena <colwanymab@gmail.com>
2025-08-26 09:42:16 +0200

    changed the notifications image

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -16,15 +16,7 @@ const Events = ({ navData }) => {
             <MainNavigation data={navData} />
             <div className='w-full mt-16 md:mt-32 flex flex-col items-center justify-start'>
                 <h1 className=' text-xl md:text-4xl xl:text-6xl font-semibold text-gray-700 text-center mt-24'>Events</h1>
-                <div className=' mt-24 mb-24 w-full flex flex-wrap justify-center gap-8 px-8'>
-                    <a href="/financial-reports">
-                        <button style={{ backgroundColor: colors.primaryColor }} 
-                        className=' font-semibold text-gray-50 min-w-64 px-8 py-2 text-sm xl:text-2xl rounded-xl cursor-pointer hover:scale-105 hover:shadow-lg transition-all ease-in-out '>Financial Reports</button>
-                    </a>
-                    <a href="/events">
-                        <button style={{ backgroundColor: colors.primaryColor }} className=' font-semibold text-gray-50 min-w-64 px-8 py-2 text-sm xl:text-2xl rounded-xl cursor-pointer hover:scale-105 hover:shadow-lg transition-all ease-in-out '>Key Events</button>
-                    </a>
-                </div>
+                
                 <div className=' w-full flex gap-4l flex-wrap mt-24 mb-48'>  
                     {
                     data?.map(({name, link}) => (
