61455ae36855c3089897c9996980ef827d914113
khomabhena <colwanymab@gmail.com>
2025-08-30 09:44:52 +0200

    rearranged services cards

--- src/data/services.js
+++ src/data/services.js
@@ -3,14 +3,9 @@ export const getServicesData = {
     subHeading: 'Loans, payments, transfers, and tools to grow, available on one platform.',
     services: [
         {
-            image: '/services/icon-01.png',
-            title: 'International Banking',
-            text: 'Easily manage global finances with secure transfers, multi-currency accounts, and expert support designed for individuals and businesses operating across borders.'
-        },
-        {
-            image: '/services/icon-02.png',
-            title: 'SME & Trade Finance',
-            text: 'Our suite of smart SME banking products will allow you the freedom of transaction, and easy access to funds for your business needs.'
+            image: '/services/icon-04.png',
+            title: 'Personal Banking',
+            text: 'Get in control of your money with GetBucks. Bill payments, airtime top up, bank to ANY Wallet and ZIPIT transactions using the mobile app.'
         },
         {
             image: '/services/icon-03.png',
@@ -18,14 +13,19 @@ export const getServicesData = {
             text: 'Get the financial boost you need, Our personal loans come with competitive rates, fast approvals, and repayment terms that suit your lifestyle.'
         },
         {
-            image: '/services/icon-04.png',
-            title: 'Personal Banking',
-            text: 'Get in control of your money with GetBucks. Bill payments, airtime top up, bank to ANY Wallet and ZIPIT transactions using the mobile app.'
+            image: '/services/icon-02.png',
+            title: 'SME & Trade Finance',
+            text: 'Our suite of smart SME banking products will allow you the freedom of transaction, and easy access to funds for your business needs.'
         },
         {
             image: '/services/icon-05.png',
             title: 'Treasury',
             text: 'Our Treasury section offers smart tools and expert services to help you manage liquidity, investments, and financial risk with confidence.'
+        },
+        {
+            image: '/services/icon-01.png',
+            title: 'International Banking',
+            text: 'Easily manage global finances with secure transfers, multi-currency accounts, and expert support designed for individuals and businesses operating across borders.'
         },
     ]
 }
